e303cf8cef6a3514eeee50a969ba567d75c0bf12
Duc Huy Nguyen Huu <nhenh2712@gmail.com>
2021-10-20 15:04:21 +0700

    refactoring the base

--- src/service/discord/discord.service.js
+++ src/service/discord/discord.service.js
@@ -1,4 +1,7 @@
 import { Client } from 'discord.js';
+import { COMMAND, COMMAND_PREFIX } from 'enum/bot-command';
+import { BOT_INTENTS } from 'enum/bot-config';
+import { BOT_EVENT } from 'enum/bot-event';
 import { GoogleService } from 'service/google';
 
 require('dotenv').config();
@@ -7,26 +10,29 @@ class DiscordServiceImpl {
     client
 
     constructor() {
-        this.client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES'] });
+        this.client = new Client({ intents: [BOT_INTENTS.GUILDS, BOT_INTENTS.GUILD_MESSAGES, BOT_INTENTS.DIRECT_MESSAGES] });
     }
 
     connectToDiscordBot() {
-        this.client.on('ready', () => {
+        this.client.on(BOT_EVENT.READY, () => {
             console.log(`Logged in as ${this.client.user.username}!`);
         });
         return this;
     }
 
     runBotService() {
-        this.client.on('messageCreate', async msg => {
+        this.client.on(BOT_EVENT.MESSAGE_CREATE, async msg => {
             if (msg.content === 'hello' || msg.content === 'hi') {
-                msg.reply('tiếng việt không nói, nói tiếng anh cc');
+                msg.reply('Tiếng việt không nói, nói tiếng anh cc');
             }
             if (msg.content === 'name?') {
                 msg.reply(this.client.user.username);
             }
-            if (msg.content === 'a') {
-                GoogleService.getGoogleSheetData(msg);
+            if (msg.content === COMMAND_PREFIX.GET_SLIDE_PREFIX + COMMAND.GET_SLIDE) {
+                const data = await GoogleService.getGoogleSheetData();
+                const toStringData = data.reduce((prev, curr) => `${prev + curr[0]}: ${curr[1]} \n`, '');
+                await msg.reply(toStringData);
+                console.log('Bot has sent the data');
             }
         });
         return this;
--- src/service/google/google.authorization.js
+++ src/service/google/google.authorization.js
@@ -18,28 +18,48 @@ export class GoogleAuthorizationImpl {
     }
 
     static getNewToken(oAuth2Client) {
-        const tokenPath = process.env.TOKEN_PATH;
         const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-        const authUrl = oAuth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: SCOPES,
-        });
-
-        console.log('Authorize by visiting this url:', authUrl);
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-        });
-
-        rl.question('Enter the code from that page here: ', code => {
-            rl.close();
+        return new Promise((resolve, reject) => {
+            const authorizeUrl = oAuth2Client.generateAuthUrl({
+                access_type: 'offline',
+                scope: SCOPES,
+            });
             try {
-                oAuth2Client.getToken(code, (authorError, oAuthToken) => {
-                    if (authorError) throw new Error('Error while trying to retrieve access token', authorError);
+                console.log('Authorize by visiting this url:', authorizeUrl);
+                const rl = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout,
+                });
+                rl.question('Enter the code from that page here: ', async code => {
+                    const oAuthToken = await oAuth2Client.getToken(code);
+                    await oAuth2Client.setCredentials(oAuthToken);
+                    resolve(oAuthToken.tokens);
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
 
-                    oAuth2Client.setCredentials(oAuthToken);
+    async authorization() {
+        const tokenPath = process.env.TOKEN_PATH;
+        const oAuth2Client = new google.auth.OAuth2(
+            this.clientId,
+            this.clientSecret,
+            this.redirectUris[0]
+        );
 
-                    fs.writeFile(tokenPath, JSON.stringify(oAuthToken), writeFileError => {
+        try {
+            const oAuthTokenFromTokenFile = await fs.promises.readFile(process.env.TOKEN_PATH);
+
+            console.log('Token file found, applying token to use Google Apis...');
+            oAuth2Client.setCredentials(JSON.parse(oAuthTokenFromTokenFile));
+
+            return oAuth2Client;
+        } catch (error) {
+            GoogleAuthorizationImpl.getNewToken(oAuth2Client)
+                .then(newOAuthToken => {
+                    fs.writeFile(tokenPath, JSON.stringify(newOAuthToken), writeFileError => {
                         console.log('Writing Token...');
                         if (writeFileError) {
                             throw new Error(writeFileError);
@@ -47,24 +67,6 @@ export class GoogleAuthorizationImpl {
                         console.log('Token has been completely written, Check the token file to get it');
                     });
                 });
-            } catch (error) {
-                console.log(error.message);
-            }
-        });
-    }
-
-    authorization(callback) {
-        const oAuth2Client = new google.auth.OAuth2(
-            this.clientId, this.clientSecret, this.redirectUris[0]
-        );
-        fs.readFile(process.env.TOKEN_PATH, (err, oAuthToken) => {
-            if (err) {
-                GoogleAuthorizationImpl.getNewToken(oAuth2Client);
-            } else {
-                console.log('Token file found, applying token to use Google Apis...');
-                oAuth2Client.setCredentials(JSON.parse(oAuthToken));
-                callback(oAuth2Client);
-            }
-        });
+        }
     }
 }
