f7b7c31f943d4fdb32b34e3b40dbec81ab2a335a
ichhoa <ichhoa129@gmail.com>
2021-11-17 19:37:42 +0700

    fix: fix production connection

--- src/core/config/knexfile.config.js
+++ src/core/config/knexfile.config.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-// import fs from 'fs';
+import fs from 'fs';
 
 dotenv.config({ path: `${__dirname}/../../../.env` });
 
@@ -22,20 +22,20 @@ module.exports = {
         },
     },
 
-    staging: {
+    production: {
         client: process.env.DB_TYPE,
         connection: {
             host: process.env.DB_HOST,
             port: process.env.DB_PORT,
             user: process.env.DB_USER,
             password: process.env.DB_PASS,
-            database: `${process.env.DB_NAME}_staging`,
+            database: process.env.DB_NAME,
             charset: 'utf8',
-            // ssl: {
-            //     require: true,
-            //     rejectUnauthorized: false,
-            //     ca: fs.readFileSync(`${process.cwd()}/ca-certificate.crt`)
-            // },
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+                ca: fs.readFileSync(`${process.cwd()}/ca-certificate.crt`)
+            },
         },
         migrations: {
             directory: `${__dirname}/../database/migrations`,
