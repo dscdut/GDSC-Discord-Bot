85f90082fec478397d2c5ea58576c2dbd358568c
ichhoa <ichhoa129@gmail.com>
2021-12-12 20:59:37 +0700

    Circleci project setup (#24)
    * Add .circleci/config.yml
    
    * infrastructure: circleci-project-setup
    
    * infrastructure: circleci-project-setup
    
    * infrastructure: circleci-project-setup
    
    * infrastructure: circleci-project-setup
    
    * infrastructure: circleci-project-setup
    
    * infrastructure: circleci-project-setup
    
    * infrastructure: circleci-project-setup
    
    * infrastructure: circleci-project-setup
    
    * infrastructure: circleci-project-setup
    
    * infrastructure: circleci-project-setup
    
    * infrastructure: circleci-project-setup

---
+++ script/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DOCKER_IMAGE="ichhoa129/discord-bot"
+
+git pull
+
+# Stop and remove the container
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+
+# Build the Docker image
+docker build -t $DOCKER_IMAGE --build-arg MACHINE_NAME=$DOCKER_IMAGE .
+docker run -p 49160:8080 -d $DOCKER_IMAGE
