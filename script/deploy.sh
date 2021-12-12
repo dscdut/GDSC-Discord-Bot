2c4810fc57af98ec521394bdc5e9b53ea7bb94a6
ichhoa <ichhoa129@gmail.com>
2021-12-12 18:12:35 +0700

    infrastructure: circleci-project-setup

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-docker-compose stop
-docker-compose rm -f
-docker-compose pull
-docker-compose up -d
+DOCKER_IMAGE="ichhoa129/discord-bot"
+
+# Stop and remove the container
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+
+# Build the Docker image
+docker build -t $DOCKER_IMAGE --build-arg MACHINE_NAME=$DOCKER_IMAGE .
+docker run -p 49160:8080 -d $DOCKER_IMAGE
