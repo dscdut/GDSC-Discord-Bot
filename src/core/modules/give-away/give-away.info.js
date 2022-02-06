24a8af368090285128a610d8aca15d5fed7f89f1
Thinh Le <roshan1234.lol@gmail.com>
2022-02-06 21:04:36 +0700

    [feat/giveaway] Hanlde multiple items when setting up giveaway event

--- src/core/modules/give-away/give-away.info.js
+++ src/core/modules/give-away/give-away.info.js
@@ -11,6 +11,6 @@ export const GiveAwayInfo = BaseModuleInfo
         {
             commandKey: COMMAND_PREFIX + COMMAND_KEY.GIVE_AWAY,
             description: 'Setting up a give-away event',
-            format: `${COMMAND_PREFIX + COMMAND_KEY.GIVE_AWAY}: [QUANTITY] >> [DATE TIME] >> [MESSAGE]`
+            format: `${COMMAND_PREFIX + COMMAND_KEY.GIVE_AWAY}: [MESSAGE] >> [HH:mm DD/MM/YYYY] >> [ITEM-1]:[QUANTITY-1], [ITEM-2]:[QUANTITY-2], ...`
         }
     ]);
--- src/core/modules/give-away/give-away.service.js
+++ src/core/modules/give-away/give-away.service.js
@@ -2,16 +2,21 @@ import { GiveAwayRequestProcess } from 'core/modules/give-away/give-away.request
 import { ScheduleService } from 'core/modules/give-away/give-away.schedule';
 import { DiscordService } from 'core/config/discord.config';
 import { formatDateForResponse, toSnippetFormat } from 'core/utils';
+import { logger } from 'package/logger';
 
 class GiveAwayServiceImpl {
     async addGiveAway(content, messageId, channelId) {
-        const giveAwayCommandProcess = new GiveAwayRequestProcess(content);
-        const data = giveAwayCommandProcess.separateData();
-        if (!data) {
-            return toSnippetFormat('Invalid command! Time cannot be set to the past & Quantity must be a positive number');
+        try {
+            const giveAwayCommandProcess = new GiveAwayRequestProcess(content);
+            const data = giveAwayCommandProcess.separateData();
+            if (!data) {
+                return toSnippetFormat('Invalid command! Time cannot be set to the past & Quantity must be a positive number');
+            }
+            ScheduleService.scheduleJob(channelId, messageId, data, DiscordService.client);
+            return toSnippetFormat(`Successfully setup giveaway event\n\n${data.message} - Time to roll: ${formatDateForResponse(data.date)}`);
+        } catch (error) {
+            logger.error(error);
         }
-        ScheduleService.scheduleJob(channelId, messageId, data, DiscordService.client);
-        return toSnippetFormat(`Successfully setup give-away event\n\n${data.message} - Time to roll: ${formatDateForResponse(data.date)}`);
     }
 }
 
